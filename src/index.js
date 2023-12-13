0e35e9b3e387ee64d00b0ccdad6a23ecf7a0c10f
matc78 <matia.cilly@gmail.com>
2023-12-13 14:32:33 +0100

    test workflows 1

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('sallut le monde !'))
 
 app.use('/user', userRouter)
 
